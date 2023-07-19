31e53ea206eccb60b0a3ba21703efe538aa93a92
danish <imdan@umich.com>
2023-07-19 14:15:13 -0400

    removed 1 project card and edited package.json

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,16 +4,16 @@ import Reveal from "./Reveal";
 
 const CardList = () => {
 
-    const desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut,"
+    const desc = "Integrates 3 functionalities: Searching workouts (MuscleWiki API), calculating daily macros (Fitness Calculator API), and calculating BMI."
 
     return (
         <>
             <Reveal>    
                 <Card top={true} title={"Workout Finder"} imgSrc={"src/assets/workout-finder-1.png"} desc={desc} tools={['React', 'HTML', 'CSS', 'NextJS', 'Vercel']}/>
             </Reveal>
-            <Reveal>
+            {/* <Reveal>
                 <Card title={"Project 2"} imgSrc={"src/assets/workout-finder-1.png"} desc={desc + desc} tools={['React', 'HTML', 'CSS', 'NextJS', 'Vercel']}/>
-            </Reveal>
+            </Reveal> */}
         </>
     )
 }
