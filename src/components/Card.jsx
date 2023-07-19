1951158aaed5d02604af10f3c52c510e25d9acbc
danish <imdan@umich.com>
2023-07-19 14:42:00 -0400

    updated url paths and removed learn more for now

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,12 @@ const Card = ({title, imgSrc, desc, tools, top=false}) => {
         <div id="card-container" className={`${top ? "mt-0" : "mt-[50px]"}`}>
             <div id="card" className="flex flex-col xl:flex-row justify-evenly pb-[50px] border-b-4 border-b-black">
                 <div id="card-image" className="w-auto xl:w-[55%] self-center mb-5 xl:mb-0">
-                    <img src={imgSrc} className="w-full h-auto" />
+                    <img src={imgSrc} alt="project image" className="w-full h-auto" />
                 </div>
                 <div id="card-desc" className="xl:w-[40%] grid">
                     <h3 className="text-2xl lg:text-6xl">{title}</h3>
                     <p className="mt-10 text-lg lg:text-xl tracking-tight h-max">{ desc.length > 190 ? desc.slice(0, 190) + "..." : desc }</p>
-                    <a href="#about" className="inline-block mt-0 text-md lg:text-xl opacity-[0.5] underline underline-offset-4 h-max w-max">Learn more</a>
+                    {/* <a href="#about" className="inline-block mt-0 text-md lg:text-xl opacity-[0.5] underline underline-offset-4 h-max w-max">Learn more</a> */}
                     <div className="mt-8 md:mt-5 text-sm lg:text-md xl:text-md 2xl:text-lg flex flex-wrap gap-4 w-full h-max self-end cursor-default">
                         {tools.map((tool) => {
                             return (
